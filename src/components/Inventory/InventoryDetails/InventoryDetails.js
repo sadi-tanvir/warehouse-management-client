e605194a48f5b8daeaedb58cc1f3c4b0d8f9f255
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-19 23:06:57 +0600

    decrement deliverd item number & update current stock

--- src/components/Inventory/InventoryDetails/InventoryDetails.js
+++ src/components/Inventory/InventoryDetails/InventoryDetails.js
@@ -3,22 +3,28 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const InventoryDetails = () => {
-    // state
-    const [fruit, setFruit] = useState({})
+  // state
+  const [fruit, setFruit] = useState({});
 
-    const { _id, name, img, description, quantity, price, supplier } = fruit;
-    // router
-    const {inventoryId} = useParams()
+//   const { _id, name, img, description, quantity, price, supplier } = fruit;
+  // router
+  const { inventoryId } = useParams();
 
-    // getting data from server
-    useEffect(() =>{
-        const getSingleInventory = async () =>{
-            const url = `http://localhost:5000/inventory/item?id=${inventoryId}`
-            const res = await axios.get(url)
-            setFruit(res.data.inventoryItem);
-        }
-        getSingleInventory()
-    })
+  // getting data from server
+  useEffect(() => {
+    const getSingleInventory = async () => {
+      const url = `http://localhost:5000/inventory/item?id=${inventoryId}`;
+      const res = await axios.get(url);
+      setFruit(res.data.inventoryItem);
+    };
+    getSingleInventory();
+  },[fruit]);
+
+  const deliveredItem = async (e) => {
+    const url = `http://localhost:5000/delivered/${inventoryId}`;
+    const res = await axios.post(url);
+    setFruit(res.data.inventoryItem);
+  };
 
   return (
     <>
@@ -27,30 +33,27 @@ const InventoryDetails = () => {
           <div className="rounded-lg shadow-lg bg-white">
             <img
               className="rounded-t-lg px-2 py-2 mx-auto"
-              src={img}
+              src={fruit?.img}
               alt="fruit pic"
             />
             <div className="p-6">
-              <h5 className="text-gray-900 text-xl font-medium mb-1">
-                {name}
-              </h5>
+              <h5 className="text-gray-900 text-xl font-medium mb-1">{fruit?.name}</h5>
               <h6 className="text-gray-900 text-xl font-medium mb-1">
-                Stock: {quantity} kg
+                Stock: {fruit?.quantity} kg
               </h6>
               <p className="text-gray-900 text-lg font-medium mb-1">
-                Price: {price} tk
+                Price: {fruit?.price} tk
               </p>
               <small className="block font-bold mb-2">
-                supplier: {supplier}
+                supplier: {fruit?.supplier}
               </small>
-              <p className="text-gray-700 text-base">
-                {description}
-              </p>
+              <p className="text-gray-700 text-base">{fruit?.description}</p>
               <button
+                onClick={deliveredItem}
                 type="button"
                 className="block w-full mx-auto py-2 font-bold bg-green-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out"
               >
-                Stock Update
+                Delivered
               </button>
             </div>
           </div>
